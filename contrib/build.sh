5c03aa1b8551a19173dcc93864894f9a1300853d
francesco <info.francescobianco@gmail.com>
2025-10-22 19:58:37 +0200

    publish

--- contrib/build.sh
+++ contrib/build.sh
@@ -5,7 +5,10 @@ set -e
 
 mkdir -p src/Illuminate/Support
 mkdir -p src/Illuminate/Collections
+mkdir -p src/Illuminate/Foundation
 
 cp laravel/framework/src/Illuminate/Support/Env.php src/Illuminate/Support/Env.php
+
 cp laravel/framework/src/Illuminate/Support/helpers.php src/Illuminate/Support/helpers.php
 cp laravel/framework/src/Illuminate/Collections/helpers.php src/Illuminate/Collections/helpers.php
+cp laravel/framework/src/Illuminate/Foundation/helpers.php src/Illuminate/Foundation/helpers.php
