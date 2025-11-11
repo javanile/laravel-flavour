c69e6c54f5da412fa54bcffc1626ce90d63e8933
francesco <info.francescobianco@gmail.com>
2025-11-11 11:36:02 +0100

    publish

--- tests/Feature/ConfigTest.php
+++ tests/Feature/ConfigTest.php
@@ -1,7 +1,7 @@
 <?php
 
 test('testing config(...) function', function () {
-    app()->instance('config', new \Illuminate\Config\Repository());
+    //app()->instance('config', new \Illuminate\Config\Repository());
     $url = 'https://example.com';
     config(['app.url' => $url]);
     expect(config('app.url'))->toBe($url);
